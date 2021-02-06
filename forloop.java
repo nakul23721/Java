d1cc5208b3f5f7563b49a4482fd62df17bb0e847
Nakul Verma <nakulverma2312@gmail.com>
2021-02-06 23:42:32 +0530

    Resolved error

--- forloop.java
+++ forloop.java
@@ -1,6 +1,8 @@
-public class Loop {
-public static void main(String[] args) {
-	for(int i=1;i<=10;i++){
+public class Loop 
+{
+	public static void main(String[] args) 
+	{
+		for(int i=1;i<=10;i++){
 		System.out.println(i);
 	}
 }
