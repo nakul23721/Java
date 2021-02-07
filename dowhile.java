03c657b2f526abdca1d4fccd2b1006d91144869e
Nakul <nakulverma2312@gmail.com>
2021-02-07 22:04:47 +0530

    Do-While Loop

---
+++ dowhile.java
@@ -0,0 +1,13 @@
+public class DoWhileLoop
+{  
+public static void main(String[] args) 
+{  
+    int i=1;  
+    do
+    {  
+        System.out.println(i);  
+    	i++;  
+    }
+    while(i<=10);  
+}  
+}  
