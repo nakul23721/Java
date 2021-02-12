4d29b0199e124ba56c3a2301e924ce40da7a6076
Nakul Verma <nakulverma2312@gmail.com>
2021-02-12 23:21:49 +0530

    Error resolved

--- palindrome.java
+++ palindrome.java
@@ -1,11 +1,13 @@
-class PalindromeExample{  
- public static void main(String args[]){  
+class Palindrome
+{  
+ public static void main(String args[])
+ {  
   int r,sum=0,temp;    
-  int n=454;//It is the number variable to be checked for palindrome  
+  int n=454; 
   
   temp=n;    
   while(n>0){    
-   r=n%10;  //getting remainder  
+   r=n%10;  
    sum=(sum*10)+r;    
    n=n/10;    
   }    
