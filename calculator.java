d53f44076d14aebc3b5ff064ef9e7106a1b6234a
Nakul Verma <nakulverma2312@gmail.com>
2021-02-14 22:46:18 +0530

    Output Error Resolved

--- calculator.java
+++ calculator.java
@@ -7,7 +7,6 @@ public class Main {
     Scanner reader = new Scanner(System.in);
     System.out.print("Enter two numbers: ");
 
-    // nextDouble() reads the next double from the keyboard
     double first = reader.nextDouble();
     double second = reader.nextDouble();
 
