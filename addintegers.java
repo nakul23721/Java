79695d0460e0b219fc2db6c4c46226ec0f8cef62
Nakul Verma <nakulverma2312@gmail.com>
2021-02-21 22:46:26 +0530

    Adding Integers Program

--- addintegers.java
+++ addintegers.java
@@ -4,8 +4,8 @@ public class AddTwoIntegers
     public static void main(String[] args) 
     {
         
-        int first = 10;
-        int second = 20;
+        int first = 30;
+        int second = 40;
 
         System.out.println("Enter two numbers: " + first + " " + second);
         int sum = first + second;
