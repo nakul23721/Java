af16a816d5767b7dece78074d8322ad6bb2e0e85
Nakul Verma <nakulverma2312@gmail.com>
2021-03-08 15:24:13 +0530

    Insertion Sort Program

--- insertionsort.java
+++ insertionsort.java
@@ -1,6 +1,6 @@
-// Java program for implementation of Insertion Sort
+
 class InsertionSort {
-    /*Function to sort array using insertion sort*/
+    
     void sort(int arr[])
     {
         int n = arr.length;
@@ -8,9 +8,7 @@ class InsertionSort {
             int key = arr[i];
             int j = i - 1;
  
-            /* Move elements of arr[0..i-1], that are
-               greater than key, to one position ahead
-               of their current position */
+            
             while (j >= 0 && arr[j] > key) {
                 arr[j + 1] = arr[j];
                 j = j - 1;
@@ -19,7 +17,7 @@ class InsertionSort {
         }
     }
  
-    /* A utility function to print array of size n*/
+    
     static void printArray(int arr[])
     {
         int n = arr.length;
@@ -29,7 +27,7 @@ class InsertionSort {
         System.out.println();
     }
  
-    // Driver method
+    
     public static void main(String args[])
     {
         int arr[] = { 12, 11, 13, 5, 6 };
