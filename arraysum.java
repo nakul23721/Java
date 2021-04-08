37f97390fa0c90784665291423cce9e455d7cc0b
Nakul Verma <nakulverma2312@gmail.com>
2021-04-08 22:10:35 +0530

    Sum of Array

--- arraysum.java
+++ arraysum.java
@@ -1,6 +1,6 @@
 public class SumOfArray {  
     public static void main(String[] args) {  
-     
+      //Initialize array 
         int [] arr = new int [] {1, 2, 3, 4, 5};  
         int sum = 0;  
        
