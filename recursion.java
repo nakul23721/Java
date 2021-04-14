499db87fc075e6c1fca1e3bf1d398e6728c7565f
Nakul Verma <nakulverma2312@gmail.com>
2021-04-14 20:54:08 +0530

    Recursion Program

--- recursion.java
+++ recursion.java
@@ -1,13 +1,18 @@
-public class Main {
+public class Main 
+{
   public static void main(String[] args) 
   {
     int result = sum(10);
     System.out.println(result);
   }
-  public static int sum(int k) {
-    if (k > 0) {
+  public static int sum(int k)
+  {
+    if (k > 0) 
+    {
       return k + sum(k - 1);
-    } else {
+    } 
+    else
+    {
       return 0;
     }
   }
