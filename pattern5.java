b41f1d32a9c97a66e0a587a21e15cde5c2046c53
Nakul Verma <nakulverma2312@gmail.com>
2021-04-19 22:56:02 +0530

    Pattern 5 Program

--- pattern5.java
+++ pattern5.java
@@ -11,7 +11,7 @@ public class Edureka
         {
             for (int j=0; j <i; j++)
             {
-                System.out.print(" ");
+                System.out.print("  ");
             }
             for (int k=i; k<=rows-1; k++) { System.out.print("*" + " "); } System.out.println(""); } for (int i= rows-1; i>= 0; i--)
         {
