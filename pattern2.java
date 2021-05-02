c26a4f8c2746c9a3f13b0eafd293eaab229e453e
Nakul Verma <nakulverma2312@gmail.com>
2021-05-02 15:27:42 +0530

    Pattern 2

--- pattern2.java
+++ pattern2.java
@@ -2,20 +2,20 @@ public class Edureka
 {  
     public static void pyramidPattern(int n) 
     {  
-        for (int i=0; i<n; i++) //outer loop for number of rows(n) { for (int j=n-i; j>1; j--) //inner loop for spaces
+        for (int i=0; i<n; i++) 
             { 
-                System.out.print(" "); //print space
+                System.out.print(" "); 
             }  
-            for (int j=0; j<=i; j++ ) //inner loop for number of columns
+            for (int j=0; j<=i; j++ ) 
             { 
-                System.out.print("* "); //print star
+                System.out.print("* "); 
             } 
   
-            System.out.println(); //ending line after each row
+            System.out.println(); 
         } 
     } 
   
-    public static void main(String args[]) //driver function
+    public static void main(String args[]) 
     { 
         int n = 5; 
         pyramidPattern(n); 
