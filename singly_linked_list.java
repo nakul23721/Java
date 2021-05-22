755b315ac05bf422ef0c28af93e25cf3583f1724
Nakul Verma <nakulverma2312@gmail.com>
2021-05-22 18:17:23 +0530

    Singly linked list Program

--- singly_linked_list.java
+++ singly_linked_list.java
@@ -1,6 +1,6 @@
 public class SinglyLinkedList {  
       
-    //Represent a node of the singly linked list  
+  
     class Node{  
         int data;  
         Node next;  
@@ -11,32 +11,32 @@ public class SinglyLinkedList {
         }  
     }  
    
-    //Represent the head and tail of the singly linked list  
+
     public Node head = null;  
     public Node tail = null;  
       
-    //addNode() will add a new node to the list  
+ 
     public void addNode(int data) {  
-        //Create a new node  
+      
         Node newNode = new Node(data);  
           
-        //Checks if the list is empty  
+      
         if(head == null) {  
-            //If list is empty, both head and tail will point to new node  
+            
             head = newNode;  
             tail = newNode;  
         }  
         else {  
-            //newNode will be added after tail such that tail's next will point to newNode  
+           
             tail.next = newNode;  
-            //newNode will become new tail of the list  
+            
             tail = newNode;  
         }  
     }  
       
-    //display() will display all the nodes present in the list  
+    
     public void display() {  
-        //Node current will point to head  
+        
         Node current = head;  
           
         if(head == null) {  
@@ -45,7 +45,7 @@ public class SinglyLinkedList {
         }  
         System.out.println("Nodes of singly linked list: ");  
         while(current != null) {  
-            //Prints each node by incrementing pointer  
+          
             System.out.print(current.data + " ");  
             current = current.next;  
         }  
@@ -56,13 +56,13 @@ public class SinglyLinkedList {
           
         SinglyLinkedList sList = new SinglyLinkedList();  
           
-        //Add nodes to the list  
+       
         sList.addNode(1);  
         sList.addNode(2);  
         sList.addNode(3);  
         sList.addNode(4);  
           
-        //Displays the nodes present in the list  
+       
         sList.display();  
     }  
 }  
