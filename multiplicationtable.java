4d810c7d68edc9541bbade670ac0a8b7ab7c688c
Nakul Verma <nakulverma2312@gmail.com>
2021-06-26 23:59:32 +0530

    Multiplication Table

--- multiplicationtable.java
+++ multiplicationtable.java
@@ -3,7 +3,7 @@ public class MultiplicationTable {
     public static void main(String[] args) {
 
         int num = 5;
-        for(int i = 1; i <= 10; ++i)
+        for(int i = 0; i <= 10; ++i)
         {
             System.out.printf("%d * %d = %d \n", num, i, num * i);
         }
